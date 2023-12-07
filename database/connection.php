3280aab7c555f82639d3a30a434831b37bd58bdd
fihar abdelali <fiharali0@gmail.com>
2023-12-07 19:40:02 +0100

    add .env

--- database/connection.php
+++ database/connection.php
@@ -1,2 +1,11 @@
 <?php
-  $conn = mysqli_connect("localhost", "root", "", "restaurant");
+
+
+require __DIR__ . '/../vendor/autoload.php'; 
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+
+
+  $conn = mysqli_connect($_ENV['hostname'], $_ENV['username'], $_ENV['password'], $_ENV['database']);
--- views/client/index.php
+++ views/client/index.php
@@ -12,7 +12,7 @@ if (isset($_POST['add'])) {
 }
 
 $commends = []; 
-if (empty($_SESSION['name'])) {
+if (!empty($_SESSION['name'])) {
     $commends=allCommend();
 // var_dump($commends);
 }
